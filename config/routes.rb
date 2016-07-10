ea39b91f138d36b58f6e057bd902a291ea7ba41f
Victor <victorsants360@gmail.com>
2016-07-10 12:55:44 -0300

    edição de publicação de sites

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "publications#index"
   resources :categories
   resources :publications
+  resources :homes, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
